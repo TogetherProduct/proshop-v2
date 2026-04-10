ceb10ecc8979794ed117fdbc71de86a72bb9000d
Tris <dinhtrisho@gmail.com>
2026-04-10 14:05:47 +0700

    Adjust admin UI, and readme instruction for item-recommendation
    adjust admin UI to remove redundant route: products, order, users.
    Change some title from 'Admin' to 'Seller' to fit the application purpose.
    update readme.md instructions for item-recommendations.
    change batchRecommendations.js python path to works on both windows and linux.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -69,16 +69,7 @@ const Header = () => {
 
               {/* Admin Links */}
               {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                  <NavDropdown.Item as={Link} to='/admin/productlist'>
-                    Products
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/admin/orderlist'>
-                    Orders
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/admin/userlist'>
-                    Users
-                  </NavDropdown.Item>
+                <NavDropdown title='Seller' id='adminmenu'>
                   <NavDropdown.Item as={Link} to='/admin/customers'>
                     Customers
                   </NavDropdown.Item>
